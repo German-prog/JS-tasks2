1829168f7dd99ef2f954c625de6d40e8e1fcdbc9
German-prog <56883343+German-prog@users.noreply.github.com>
2020-03-01 13:27:04 +0200

    Add files via upload

---
+++ JS-TASK3/scripts/index.js
@@ -0,0 +1,24 @@
+const PEOPLE = 2;
+const MONEY = 15000;
+const TREES = 4;
+const CARS = 11;
+const DAYS = 7;
+
+
+console.log(PEOPLE)
+console.log(MONEY);
+console.log(TREES);
+console.log(CARS);
+console.log(DAYS);
+
+
+let WORLD = "WORLD";
+let LIFE = "LIFE";
+let SMILE = "SMILE";
+let SUMMER = "SUMMER";
+let MARCH = "MARCH";
+console.log(WORLD);
+console.log(LIFE);
+console.log(SMILE);
+console.log(SUMMER);
+console.log(MARCH);
